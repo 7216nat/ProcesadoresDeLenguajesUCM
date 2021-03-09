ee4b92afb9c70b0318adb1bb6dea942f598735e9
101001000 <kike.delacalle.montilla@gmail.com>
2021-03-09 01:18:10 +0100

    Add files via upload

---
+++ Analizador/src/Estado.java
@@ -0,0 +1,16 @@
+
+public enum Estado {
+
+	INICIO(null), ENTERO(CL.ENTERO), MAS(CL.MAS), REAL(CL.REAL), EOF(CL.EOF), VAR(CL.VAR),
+	POR(CL.POR), DIV(CL.DIV), PAP(CL.PAP), SEP(CL.SEP),
+	PCIE(CL.PCIE), GT(CL.GT), GE(CL.GE), LT(CL.LT), LE(CL.LE),
+	ASIG(CL.ASIG), EQ(CL.EQ), NE(CL.NE), PC(CL.PC),
+	MENOS(CL.MENOS), CERO(CL.ENTERO), REAL1(CL.REAL), REAL2(CL.REAL), REAL3(CL.REAL);
+	
+	CL clase;
+	
+	Estado(CL clase) {
+		this.clase = clase;
+	}
+	
+}
